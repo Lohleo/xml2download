0c0e8c492c9995d1d7197efeb37bc167c2f5c41d
Lohleo <151208213+Lohleo@users.noreply.github.com>
2023-11-18 21:12:45 +0100

    try xml2js - brocken

--- code.links.js
+++ code.links.js
@@ -1,7 +1,10 @@
 
  var fs = require('fs'),
  request = require('request');
+ const util = require('util');
+ xml2js = require('xml2js');
 
+ var parser = new xml2js.Parser();
 
  var download = function(uri, filename, callback){
  request.head(uri, function(err, res, body){
@@ -12,12 +15,13 @@
  });
 };
 
-xml2js = require('xml2js');
 
-var parser = new xml2js.Parser();
-fs.readFile(__dirname + '/foo.xml', function(err, data) {
+fs.readFile(__dirname + '/resources/input.xml', function(err, data) {
     parser.parseString(data, function (err, result) {
         console.dir(result);
+        console.log(util.inspect(result, false, null))
+        var obj = JSON.parse(util.inspect(result, false, null))
+        console.log(obj.verzeichnis.eintrag.url)
         console.log('Done');
     });
 });
