3498608cca5b4770305bc0caa7f5c01dca38a411
Alestix <johndoe@example.com>
2023-11-18 22:06:50 +0100

    parse xml with xmldom

--- code.links.js
+++ code.links.js
@@ -1,27 +1,32 @@
 
- var fs = require('fs'),
- request = require('request');
- const util = require('util');
- xml2js = require('xml2js');
+var fs = require('fs'),
+    request = require('request');
 
- var parser = new xml2js.Parser();
+const { DOMParser } = require('xmldom')
 
- var download = function(uri, filename, callback){
- request.head(uri, function(err, res, body){
- console.log('content-type:', res.headers['content-type']);
- console.log('content-length:', res.headers['content-length']);
+var download = function (uri, filename, callback) {
+    request.head(uri, function (err, res, body) {
+        console.log('content-type:', res.headers['content-type']);
+        console.log('content-length:', res.headers['content-length']);
 
- request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
- });
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    });
 };
 
 
-fs.readFile(__dirname + '/resources/input.xml', function(err, data) {
-    parser.parseString(data, function (err, result) {
-        console.dir(result);
-        console.log(util.inspect(result, false, null))
-        var obj = JSON.parse(util.inspect(result, false, null))
-        console.log(obj.verzeichnis.eintrag.url)
-        console.log('Done');
-    });
+
+fs.readFile(__dirname + '/resources/input.xml', 'utf-8', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    var 
+        url,
+        doc;
+    doc = new DOMParser().parseFromString(data, 'application/xml');
+
+    url =  Array.from(doc.getElementsByTagName('url'));
+    // console.log(url)
+    url.forEach(v => console.log(v.firstChild.data));
+    //url.forEach(v => download(v.firstChild.data));
+
 });
